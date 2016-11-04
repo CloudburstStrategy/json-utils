6fc88dba4dc3ff68c36be0bfac0c209f88330345
Adam Chesney <ajchesney@gmail.com>
2016-11-04 13:06:56 +0000

    added guava for immutable collections

--- src/main/java/com/paxport/json/JsonMapper.java
+++ src/main/java/com/paxport/json/JsonMapper.java
@@ -4,12 +4,11 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.guava.GuavaModule;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -51,6 +50,7 @@ public class JsonMapper {
     public static void registerModules(ObjectMapper objectMapper) {
         objectMapper.registerModule(new Jdk8Module());
         objectMapper.registerModule(javaTimeModule());
+        objectMapper.registerModule(new GuavaModule());
     }
 
     private static JavaTimeModule javaTimeModule(){
